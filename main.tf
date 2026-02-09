e307915be12319c7b0d6acfdda29f67fb1d5aaf3
Phong Nguyen <phongnguyen@unknowndedd52aedcef.attlocal.net>
2026-02-09 07:09:34 -0600

    changed security group id attribute to string

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "app_server" {
   instance_type = var.instance_type
   associate_public_ip_address = true
 
-  vpc_security_group_ids = [data.tfe_outputs.source_workspace.nonsensitive_values.vpc_security_group_ids]
+  vpc_security_group_ids = data.tfe_outputs.source_workspace.nonsensitive_values.vpc_security_group_ids
   subnet_id              = data.tfe_outputs.source_workspace.nonsensitive_values.vpc_subnet[0]
 
   tags = {
